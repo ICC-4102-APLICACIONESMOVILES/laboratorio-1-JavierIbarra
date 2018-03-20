b83277e0085cb12c612add97e1795fcc2f42af6a
DESKTOP-8E5E3T5\jaiba <jaibarra1@miuandes.cl>
2018-03-20 12:15:34 -0300

    Lab01

---
+++ Laboratorio01/app/src/main/java/com/example/jaiba/laboratorio01/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jaiba.laboratorio01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Logear(View view){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String result=data.getStringExtra("result");
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
+}
